0562630040fcb3d0a9cdccac101b3d5497bf6b37
misostack <misostack.com@gmail.com>
2022-09-10 00:14:45 +0700

    init layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,21 @@
 export default function Home() {
   return (
     <>
-      <h1>SONNM.COM</h1>
-      <p>
-        Công việc hằng ngày của tôi là sáng tạo những phương thức mới, nhằm thúc
-        đẩy kết nối giữa người tiêu dùng với các nhà cung cấp sản phẩm/dịch vụ
-        tốt nhất.
-      </p>
+      <div className="container">
+        <h1>SONNM.COM</h1>
+        <p>
+          Công việc hằng ngày của tôi là sáng tạo những phương thức mới, nhằm
+          thúc đẩy kết nối giữa người tiêu dùng với các nhà cung cấp sản
+          phẩm/dịch vụ tốt nhất.
+        </p>
+
+        <blockquote className="p-4 my-4 bg-gray-50 border-l-4 border-gray-300 dark:border-gray-500 dark:bg-gray-800">
+          <p className="text-xl italic font-medium leading-relaxed text-gray-900 dark:text-white">
+            Phương châm của tôi khi gặp khách hàng là đem đến cho họ sản
+            phẩm/dịch vụ phù hợp.
+          </p>
+        </blockquote>
+      </div>
     </>
   );
 }
