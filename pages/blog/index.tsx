8ef13b0a8893ee598d0c5a6ca4035ba07080ce9a
misostack <misostack.com@gmail.com>
2022-09-16 14:38:20 +0700

    add blog layout

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -5,26 +5,28 @@ import Link from "next/link";
 export default () => {
   return (
     <>
-      <h2>List Category</h2>
-      <ul>
-        {Array.from(new Array(6)).map((_, idx) => (
-          <li>
-            <Link href={`/blog/category-slug-${++idx}`}>
-              <a>Category {`${idx}`}</a>
-            </Link>
-          </li>
-        ))}
-      </ul>
-      <h2>List Post</h2>
-      <ul>
-        {Array.from(new Array(6)).map((_, idx) => (
-          <li>
-            <Link href={`/blog/posts/${++idx}`}>
-              <a>Post {`${idx}`}</a>
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <div className="container">
+        <h2>List Category</h2>
+        <ul>
+          {Array.from(new Array(6)).map((_, idx) => (
+            <li>
+              <Link href={`/blog/category-slug-${++idx}`}>
+                <a>Category {`${idx}`}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <h2>List Post</h2>
+        <ul>
+          {Array.from(new Array(6)).map((_, idx) => (
+            <li>
+              <Link href={`/blog/posts/${++idx}`}>
+                <a>Post {`${idx}`}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 };
